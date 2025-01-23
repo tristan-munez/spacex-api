9c13ea0a5be908f3ed1ae78db38f8d2fd8099861
Tristan <142881486+dunkwad-tm@users.noreply.github.com>
2025-01-24 04:51:51 +0800

    adjusted excessively large timout value

--- src/api/launches.js
+++ src/api/launches.js
@@ -3,15 +3,15 @@ import axios from 'axios';
 // Create an Axios instance with the SpaceX API base URL
 const apiClient = axios.create({
   baseURL: 'https://api.spacexdata.com/v3/launches',
-  timeout: 1000000, //To show loading
+  timeout: 30000, //To show loading
 });
 
 export const fetchLaunches = async (offset = 0) => {
-    try {
-      const response = await apiClient.get('/', { params: { limit: 5, offset } });
-      return response.data;
-    } catch (error) {
-      console.error('Error fetching launches:', error);
-      throw error;
-    }
+  try {
+    const response = await apiClient.get('/', { params: { limit: 5, offset } });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching launches:', error);
+    throw error;
+  }
 };
